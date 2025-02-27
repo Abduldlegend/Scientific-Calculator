d3da087641a6163dfd7e06adc1308861cb85b093
Abdulkadir <musaabdulkadir90@gmail.com>
2025-02-28 00:35:39 +0100

    updated style

--- script.js
+++ script.js
@@ -28,8 +28,25 @@ function calculateResult(){
         
         expression = expression.replace(/tan\((.*?)\)/g, (_, expr) => `Math.tan((${expr}) * Math.PI / 180)`);
 
+        expression = expression.replace(/ln/g, "Math.log");
+
+        expression = expression.replace(/3.142/g, "Math.PI");
+
+        expression = expression.replace(/(\d+\.?\d*)[eE](\d+)/g, (_, base, exponent) => `${base} * Math.pow(10, ${exponent})`);
+
+        expression = expression.replace(/(\d+)!/g, (_, num) => factorial(parseInt(num)));
+
         inputField.value = eval(expression);
     } catch (error) {
         inputField.value = "Error";
+    }
+
+    function factorial(n) {
+        if (n === 0 || n === 1) return 1;
+        let result = 1;
+        for (let i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
